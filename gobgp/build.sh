4af09b2315a178709433bd47656498a9e52af2b6
Chun Chen <chenchun.feed@gmail.com>
2018-08-14 01:21:31 +0800

    Add quagga gobgp dockerfile

---
+++ gobgp/build.sh
@@ -0,0 +1,2 @@
+docker build --network=host -t chenchun/gobgp .
+# docker run -d --privileged gobgp
---
+++ quagga/build.sh
@@ -0,0 +1,2 @@
+docker build --network=host -t chenchun/quagga .
+# docker run -d --privileged quagga
